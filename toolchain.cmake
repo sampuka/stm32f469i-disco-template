4279c4bb2361ec7393591c77aed284ff1a554c78
Mathias Ebbensgaard Jensen <matddi@gmail.com>
2023-09-04 20:15:56 +0200

    Cleanup

--- toolchain.cmake
+++ toolchain.cmake
@@ -5,7 +5,7 @@ set(CMAKE_C_COMPILER arm-none-eabi-gcc)
 set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
 set(CMAKE_ASM_COMPILER arm-none-eabi-gcc)
 
-set(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs -Wl,--no-warn-rwx-segment -Wl,-Map=main.map -Wl,--gc-sections --static -nostartfiles -L.. -Tstm32-biggest.ld" CACHE INTERNAL "")
+set(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs -Wl,--no-warn-rwx-segment -Wl,-Map=main.map -Wl,--gc-sections --static -nostartfiles -L../startup -Tstm32-biggest.ld" CACHE INTERNAL "")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
