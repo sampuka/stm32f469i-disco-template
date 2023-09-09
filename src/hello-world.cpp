4a4b999d84bfbd668febfa97799bb8681f6f9aff
Mathias Ebbensgaard Jensen <matddi@gmail.com>
2023-09-09 19:38:12 +0200

    Using TIM2 to blink LD2 (poorly)

--- src/hello-world.cpp
+++ src/hello-world.cpp
@@ -1,10 +1,13 @@
+#include "bsp_devtemp.hpp"
 #include "bsp_init.hpp"
 #include "bsp_leds.hpp"
 
 #include <cstdint>
 
 volatile uint32_t var = 0;
+volatile uint32_t var2 = 0;
 volatile bool state = false;
+volatile bool state2 = false;
 
 int main()
 {
@@ -19,10 +22,15 @@ int main()
 	{
 		var = var + 1;
 
+		var2 = bsp::get_tim2_counter();
+
 		if ((var % 100000) == 0)
 		{
 			state = !state;
 			bsp::set_ld4(state);
+
+			state2 = (var2 % 2) == 0;
+			bsp::set_ld2(state2);
 		}
 	}
 }
